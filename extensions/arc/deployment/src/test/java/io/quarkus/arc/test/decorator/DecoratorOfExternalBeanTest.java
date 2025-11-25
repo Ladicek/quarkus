46e725dca3efb2b434076f5582592d3f7d738115
Ladislav Thon <lthon@redhat.com>
2025-11-25 09:31:51 +0100

    ArC: fix injection into non-app beans with app decorators
    A while ago, we turned generated `_Bean`, `_Subclass` and `_ClientProxy`
    classes pertaining to non-app beans into app classes to allow decoration
    by app decorators. This in turn made injection into non-`public` IPs
    impossible.
    
    We fix it in this commit by a bytecode transformation that makes these IPs
    `public`. However, this might break subclasses (albeit in relatively rare
    cases), so in case the bean class has subclasses, we error out.
    
    Also, this commit turns the generated `_ClientProxy` classes back into
    non-app, because that allows invoking non-`public` methods. Turning
    the generated `_ClientProxy` class into app class was arguably a mistake.
    
    Finally, this commit removes the `<scope>test</scope>` on 2 dependencies
    in the `extensions/arc/deployment` module, because when test-scoped
    dependencies are used in `QuarkusUnitTest.setForcedDependencies()`,
    they are treated as application archives. If they have the default scope,
    they are treated as non-app archives, which is exactly what we need
    in all tests in this module that use `setForcedDependencies()`. To make
    sure we don't regress again, a comment is added in the POM and all tests
    that use this feature also assert that the classes from the external
    archives are indeed loaded by the base runtime CL (and not runtime CL).

--- extensions/arc/deployment/src/test/java/io/quarkus/arc/test/decorator/DecoratorOfExternalBeanTest.java
+++ extensions/arc/deployment/src/test/java/io/quarkus/arc/test/decorator/DecoratorOfExternalBeanTest.java
@@ -1,18 +1,25 @@
 package io.quarkus.arc.test.decorator;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 
 import jakarta.decorator.Decorator;
 import jakarta.decorator.Delegate;
+import jakarta.enterprise.event.Event;
+import jakarta.enterprise.inject.Instance;
 import jakarta.inject.Inject;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
+import io.quarkus.arc.test.supplement.ConsumerOfSomeBeanInExternalLibrary;
 import io.quarkus.arc.test.supplement.SomeBeanInExternalLibrary;
+import io.quarkus.arc.test.supplement.SomeEventInExternalLibrary;
 import io.quarkus.arc.test.supplement.SomeInterfaceInExternalLibrary;
+import io.quarkus.arc.test.supplement.SomeProducedDependencyInExternalLibrary;
 import io.quarkus.builder.Version;
 import io.quarkus.maven.dependency.Dependency;
 import io.quarkus.test.QuarkusUnitTest;
@@ -30,9 +37,70 @@ public class DecoratorOfExternalBeanTest {
     @Inject
     SomeBeanInExternalLibrary bean;
 
+    @Inject
+    ConsumerOfSomeBeanInExternalLibrary consumer;
+
+    @Inject
+    Event<SomeEventInExternalLibrary> event;
+
+    @Inject
+    Instance<SomeProducedDependencyInExternalLibrary> instance;
+
     @Test
     public void test() {
         assertEquals("Delegated: Hello", bean.hello());
+
+        assertFalse(SomeBeanInExternalLibrary.pinged);
+        assertFalse(SomeBeanInExternalLibrary.observed);
+        assertFalse(SomeBeanInExternalLibrary.produced);
+        assertFalse(SomeBeanInExternalLibrary.disposed);
+
+        assertEquals("pong", consumer.ping());
+
+        assertTrue(SomeBeanInExternalLibrary.pinged);
+        assertFalse(SomeBeanInExternalLibrary.observed);
+        assertFalse(SomeBeanInExternalLibrary.produced);
+        assertFalse(SomeBeanInExternalLibrary.disposed);
+
+        event.fire(new SomeEventInExternalLibrary());
+
+        assertTrue(SomeBeanInExternalLibrary.pinged);
+        assertTrue(SomeBeanInExternalLibrary.observed);
+        assertFalse(SomeBeanInExternalLibrary.produced);
+        assertFalse(SomeBeanInExternalLibrary.disposed);
+
+        SomeProducedDependencyInExternalLibrary dependency = instance.get();
+
+        assertTrue(SomeBeanInExternalLibrary.pinged);
+        assertTrue(SomeBeanInExternalLibrary.observed);
+        assertTrue(SomeBeanInExternalLibrary.produced);
+        assertFalse(SomeBeanInExternalLibrary.disposed);
+
+        assertEquals("Produced: Hello", dependency.hello());
+
+        assertTrue(SomeBeanInExternalLibrary.pinged);
+        assertTrue(SomeBeanInExternalLibrary.observed);
+        assertTrue(SomeBeanInExternalLibrary.produced);
+        assertFalse(SomeBeanInExternalLibrary.disposed);
+
+        instance.destroy(dependency);
+
+        assertTrue(SomeBeanInExternalLibrary.pinged);
+        assertTrue(SomeBeanInExternalLibrary.observed);
+        assertTrue(SomeBeanInExternalLibrary.produced);
+        assertTrue(SomeBeanInExternalLibrary.disposed);
+    }
+
+    @Test
+    public void testNonAppArchive() {
+        assertTrue(SomeBeanInExternalLibrary.class.getClassLoader().getName()
+                .contains("Quarkus Base Runtime ClassLoader"));
+        assertTrue(ConsumerOfSomeBeanInExternalLibrary.class.getClassLoader().getName()
+                .contains("Quarkus Base Runtime ClassLoader"));
+        assertTrue(SomeEventInExternalLibrary.class.getClassLoader().getName()
+                .contains("Quarkus Base Runtime ClassLoader"));
+        assertTrue(SomeProducedDependencyInExternalLibrary.class.getClassLoader().getName()
+                .contains("Quarkus Base Runtime ClassLoader"));
     }
 
     @Decorator
